7b63ff952a45de0e520cf8a5c917f55c9c8b38c6
irinacasa98-cmd <irinacasa98@gmail.com>
2026-05-01 11:37:46 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import urllib.parse
 
 # --- CONFIGURACIÓN ---
 # Este es el corazón del botón, no lo borres
-LINK_CITAS_GOOGLE = "https://calendar.app.google/SKMUag2UBbHP52Lh9"
+LINK_CITAS_GOOGLE = "https://calendar.google.com/calendar/appointments/schedules/AcZssZ3-lDy6ICRk0OrhYm2IxKSub_XKS-d-BijdvSK77zL1CcXgAfTTsIVtjw46IKE42NYAjy5QOp4h?gv=true"
 
 st.set_page_config(page_title="Turnos - Nails by Irina", layout="wide", page_icon="💅")
 
